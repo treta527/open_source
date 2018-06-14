0742ce89ba497a48b6aa26e8d6662619ebd200ae
Gonzalo Pena-Castellanos <goanpeca@gmail.com>
2018-06-14 16:50:37 -0500

    Add stubs

--- open_source/open_source.py
+++ open_source/open_source.py
@@ -1,3 +1,15 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+
+def check_palindrome(word):
+    """Check if a word is a palindrome."""
+
+
+def random_letter():
+    """Return a random letter of the alphabet."""
+
+
+def random_int(lower, upper):
+    """Return a random integer from the alphabet."""
