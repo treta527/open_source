434c4f7ec0fdcaf6521c97405d4667c979114d98
Gonzalo Pena-Castellanos <goanpeca@gmail.com>
2018-06-14 17:01:06 -0500

    Fix ci

--- open_source/open_source.py
+++ open_source/open_source.py
@@ -1,14 +1,26 @@
 # -*- coding: utf-8 -*-
-
 """Main module."""
+
+# Standard library imports
+import random
+import string
 
 
 def check_palindrome(word):
     """Check if a word is a palindrome."""
+    reverse = word[::-1]
+    return reverse == word
 
 
-def random_letter():
+def random_letter(word=None):
     """Return a random letter of the alphabet."""
+    if word:
+        letters = string.ascii_letters
+    else:
+        letters = word
+
+    index = random.randint(0, len(letters))
+    return letters[index]
 
 
 def random_int(lower, upper):
